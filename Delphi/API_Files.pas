24af0255e6c62c7c80b951e0469b7ddd1275e41d
alezzle <niekras@tut.by>
2017-06-05 17:37:31 +0300

    dev

--- Delphi/API_Files.pas
+++ Delphi/API_Files.pas
@@ -6,6 +6,7 @@ type
   TFilesEngine = class
   public
     class function GetTextFromFile(aFileName: String): String;
+    class procedure SaveTextToFile(aFileName, aText: String);
   end;
 
 implementation
@@ -13,6 +14,19 @@ implementation
 uses
   System.Classes,
   System.SysUtils;
+
+class procedure TFilesEngine.SaveTextToFile(aFileName, aText: String);
+var
+  SL: TStringList;
+begin
+  SL := TStringList.Create;
+  try
+    SL.Text := aText;
+    SL.SaveToFile(aFileName);
+  finally
+    SL.Free;
+  end;
+end;
 
 class function TFilesEngine.GetTextFromFile(aFileName: string): string;
 var
